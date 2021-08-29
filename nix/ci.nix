e4ef0e8d8a6ddb838c26e198351d2a8147bbc79e
Julian Stecklina <js@alien8.de>
2021-08-29 22:58:31 +0200

    Introduce module and test

--- nix/ci.nix
+++ nix/ci.nix
@@ -1,6 +1,28 @@
 { nixpkgs ? (import ./sources.nix).nixpkgs }:
 let
   pkgs = import nixpkgs {};
-in {
+
   tuxedo-control-center = pkgs.callPackage ./tuxedo-control-center {};
+in {
+  inherit tuxedo-control-center;
+
+  test = pkgs.nixosTest ({ lib, pkgs, ... }: {
+    name = "tuxedo-control-center-test";
+    machine = { pkgs, ... }: {
+      imports = [
+        ./module.nix
+      ];
+
+      nixpkgs.overlays = [
+        (final: prev: { inherit tuxedo-control-center; })
+      ];
+
+      hardware.tuxedo-control-center.enable = true;
+    };
+
+    testScript = ''
+      start_all();
+      machine.wait_for_unit("tccd.service")
+    '';
+  });
 }
