370d4e925ee85b80e4e5a2b62d210d34a49f6e7a
Julian Stecklina <js@alien8.de>
2022-06-05 09:16:20 +0200

    ci: update to NixOS 22.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
